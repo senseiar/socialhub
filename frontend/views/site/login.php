3f7264b878a1de9a47b352b1a14822f14467d255
Andrey Ratkin <aratkin77@gmail.com>
2018-07-10 22:45:04 +0300

    facebook login

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -35,5 +35,12 @@ $this->params['breadcrumbs'][] = $this->title;
 
             <?php ActiveForm::end(); ?>
         </div>
+            <div class="col-lg-5">
+            <h4>Via Social Nrtworks</h4>
+            <?= yii\authclient\widgets\AuthChoice::widget([
+                'baseAuthUrl' => ['site/auth'],
+                'popupMode' => false,
+            ]) ?>
+            </div>
     </div>
 </div>
